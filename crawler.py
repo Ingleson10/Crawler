e120e6bab535ffa16a768ef33c4efe80f928e0d0
Ingleson10 <erik.ingleson10.ei@gmail.com>
2024-03-15 14:45:52 -0300

    Aprimorando funções e melhorando os textos salvos em PDFno padrão UTF-8

--- crawler.py
+++ crawler.py
@@ -9,7 +9,6 @@ load_dotenv()
 
 chatgpt_key = os.getenv('CHATGPT_KEY')
 
-
 class WebsiteCrawler:
     def __init__(self, url, output_file, api_key=chatgpt_key, max_chars=2000):
         self.url = url
@@ -17,18 +16,28 @@ class WebsiteCrawler:
         self.output_file = output_file
         self.max_chars = max_chars
 
+    def crawl(self, ai_command):
+        print('Executando...')
+        text_buscado = self.busca_dados()
+        text_processado = self.processa_dados(text_buscado)
+        if text_processado:
+            text_analisado = self.analisa_dados(ai_command, text_processado)
+            self.salva_dados(text_analisado)
+        
+        print('Finalizado!!!')
+
     def busca_dados(self):
+        print('Buscando...')
         try:
             response = requests.get(self.url)
+            response.encoding = response.apparent_encoding
             response.raise_for_status()
             return response.text
         except Exception as e:
-            return f"Erro ao buscar dados: {str(e)}"
+            return f"Erro ao buscar dados: {str(e)}"  
 
-    def analisa_dados(self, result_content):
-        pass
-
-    def processa_dados(self, ai_command, text):
+    def processa_dados(self, text):
+        print('Processando...')
         try:
             soup = BeautifulSoup(text, 'html.parser')
             maintag = soup.find('main')
@@ -40,36 +49,37 @@ class WebsiteCrawler:
             if len(text) > self.max_chars:
                 text = text[:self.max_chars]
 
-            client = OpenAI(api_key=self.api_key)
-            completion = client.chat.completions.create(
+            return text
+
+        except Exception as e:
+            return f"Erro ao processar os dados: {str(e)}"
+
+    def analisa_dados(self, ai_command, text):
+        print('Analisando...')
+        client = OpenAI(api_key=self.api_key)
+        completion = client.chat.completions.create(
                 model="gpt-3.5-turbo", messages=[
                     {"role": "system", "content": ai_command},
                     {"role": "user", "content": text}
                 ])
 
-            result_content = completion.choices[0].message
-            return result_content.content
-
-        except Exception as e:
-            return f"Erro ao processar os dados: {str(e)}"
+        result_content = completion.choices[0].message
+        return result_content.content
 
     def salva_dados(self, result_content):
+        print('Salvando...')
         try:
             path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+            options = {
+                'encoding': 'UTF-8',
+                'enable-local-file-access': True
+            }
             config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-            pdfkit.from_string(result_content, f'pdf/{self.output_file}', configuration=config)
+            pdfkit.from_string(result_content, f'pdf/{self.output_file}', configuration=config, options=options)
 
             return "Dados salvos com sucesso!!!"
 
         except Exception as e:
             return f"Erro ao salvar os dados: {str(e)}"
-
-    def crawl(self, ai_command):
-        text = self.busca_dados()
-        result_content = self.processa_dados(ai_command, text)
-        if result_content:
-            self.analisa_dados(result_content)
-            return self.salva_dados(result_content)
-        return "Nenhum conteúdo processado."
 
 
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from crawler import WebsiteCrawler
 
-meucrawler = "Seja um analisador de textos e responda somente as categorias do que se trata o texto no formato de tags com hashtags"
 minhaurl = 'https://docs.python.org/pt-br/3/tutorial/index.html'
-crawler1 = WebsiteCrawler(minhaurl, 'output_meucrawler.pdf')
-resultado = crawler1.crawl(meucrawler)
 
-meuprofessor = 'Seja meu professor de python'
+'''meucrawler = "Seja um analisador de textos e responda somente as categorias do que se trata o texto no formato de tags com hashtags"
+crawler1 = WebsiteCrawler(minhaurl, 'output_meucrawler.pdf')
+resultado = crawler1.crawl(meucrawler)'''
+
+meuprofessor = 'Seja meu professor de python e faça um resumo do texto'
 crawler2 = WebsiteCrawler(minhaurl, 'output_meuprofessor.pdf')
 result = crawler2.crawl(meuprofessor)
-
-print(resultado, result)
