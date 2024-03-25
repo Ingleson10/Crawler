64844ad53ea34fe43b93e2a6f109438ee6cb9996
Ingleson10 <erik.ingleson10.ei@gmail.com>
2024-03-25 15:51:02 -0300

    Atualizando dados na main

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from crawler import WebsiteCrawler
 
-minhaurl = 'https://docs.python.org/pt-br/3/tutorial/index.html'
+def main():
+    # URL que será rastreada
+    minha_url = 'https://docs.python.org/pt-br/3/tutorial/index.html'
+    
+    # Inicializa o rastreador do site
+    crawler = WebsiteCrawler()
 
-crawler = WebsiteCrawler()
-
-meucrawler = "Seja um analisador de textos e responda somente as categorias do que se trata o texto no formato de tags com hashtags"
-crawler.crawl(meucrawler, minhaurl, 'output_meucrawler.pdf')
+    # Configuração do primeiro rastreador
+    meucrawler_config = {
+        'description': "Seja um analisador de textos e responda somente as categorias do que se trata o texto no formato de tags com hashtags",
+        'output_file': 'output_meucrawler.pdf'
+    }
+    # Executa o rastreamento
+    crawler.crawl(minha_url, **meucrawler_config)
 
 '''meuprofessor = 'Seja meu professor de python e faça um resumo do texto'
 crawler.crawl(meuprofessor, minhaurl, 'output_meuprofessor.pdf')'''
